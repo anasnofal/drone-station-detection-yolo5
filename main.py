c234f9699c5ae87f1f49a212b86d50e443c9bc52
anas <anasnofal14@yahoo.com>
2022-10-19 12:51:57 +0200

    with threshthreshold

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import torch
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
-#LOding the model 
+#Loding the model 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='D:/drone/best.pt', force_reload=True)
-
+model.conf = 0.9  # confidence threshold (0-1)
+model.iou = 0.6  # NMS IoU threshold (0-1)  
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
     
     # Make detections 
     results = model(frame)
-    
     cv2.imshow('YOLO', np.squeeze(results.render()))
     
     if cv2.waitKey(10) & 0xFF == ord('q'):
